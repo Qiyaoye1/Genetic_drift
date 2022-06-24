80640e259d80deff25cb3f86cc6d6e8c34dd312e
Qiyaoye1 <78765206+Qiyaoye1@users.noreply.github.com>
2022-06-24 15:39:01 +0200

    Delete Package_dependencies.R

--- Package_dependencies.R
+++
@@ -1,6 +0,0 @@
-packages <- c("writexl", "readxl", "foreach", "ggplot2", "reshape2", "rstudioapi")
-
-for (package in packages){
-    if (!require("BiocManager", quietly = TRUE))
-        install.packages("BiocManager")
-}
